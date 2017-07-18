43824db339289f5077ad4744f304d8bfcc78f006
enginyilmz <enginyilmaz.math@hotmail.com>
2017-07-18 10:08:21 +0300

    Add welcome page.

---
+++ LoginWithCookie/Welcome.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginWithCookie
+{
+    public partial class Welcome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                this.lbl_username.Text = Session["username"].ToString();
+            }
+        }
+        protected void lb_logout_Click(object sender, EventArgs e)
+        {
+            //Remove Sessio
+            Session.Remove("username");
+            //Remove Cookies
+            HttpCookie ckUsername = new HttpCookie("username");
+            ckUsername.Expires = DateTime.Now.AddDays(-1d);
+            Response.Cookies.Add(ckUsername);
+
+            HttpCookie ckPassword = new HttpCookie("password");
+            ckPassword.Expires = DateTime.Now.AddDays(-1d);
+            Response.Cookies.Add(ckPassword);
+
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
